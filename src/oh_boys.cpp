5306ed69bb14fa3c0ce11c34561e2ef12d38b398
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-03-04 18:22:17 +0100

    Add CLI with Lyra

--- src/oh_boys.cpp
+++ src/oh_boys.cpp
@@ -6,41 +6,71 @@
 #include <random>
 #include <vector>
 
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+#include <lyra/lyra.hpp>
+
 #include "Fnx.hpp"
 
-int
-main(int argc, char* argv[])
-{
-    if (argc != 3)
-    {
-        std::cout << "usage:\n  " << argv[0] << " <order> <npoints>" << std::endl;
-        std::cout << "<order> (integer) order of the Boys function" << std::endl;
-        std::cout << "<npoints> (integer) number of points at which to evaluate the Boys function" << std::endl;
-    }
-    else
-    {
-        auto order   = std::stoi(argv[1]);
-        auto npoints = std::stoi(argv[2]);
+int main(int argc, char *argv[]) {
+  // CLI set up
+  int32_t order;
+  auto npoints = 1e7;
+  auto lower = 0.0;
+  auto upper = 125.0;
+  bool show_help = false;
 
-        auto lower = 0.0;
-        auto upper = 125.0;
+  auto cli = lyra::help(show_help) |
+             lyra::arg(order, "order")
+                 .required()("Evaluation order of the Boys function") |
+             lyra::opt(npoints, "npoints")["-n"]["--npoints"](
+                 "How many evaluation points (default 1e7)") |
+             lyra::opt(lower, "lower")["-l"]["--lower"](
+                 "Lower bound of evaluation interval (default 0.0)") |
+             lyra::opt(upper, "upper")["-u"]["--upper"](
+                 "Upper bound of evaluation interval (default 125.0)");
 
-        std::cout << "Evaluating the Boys function of order " << order << " on " << npoints << " random points in [" << lower << ", " << upper << "]" << std::endl;
+  // Parse the program arguments:
+  auto result = cli.parse({argc, argv});
 
-        std::random_device                     rd;
-        std::mt19937                           mt(rd());
-        std::uniform_real_distribution<double> dist(lower, upper);
+  // Check that the arguments where valid:
+  if (!result) {
+    std::cerr << "Error in command line: " << result.errorMessage()
+              << std::endl;
+    std::cerr << cli << "\n";
+    return EXIT_FAILURE;
+  }
 
-        auto v = std::vector<double>(npoints, 0.0);
-        std::generate(v.begin(), v.end(), [&dist, &mt]() { return dist(mt); });
-
-        auto start = std::chrono::steady_clock::now();
-        auto boys  = boys_function(order, v);
-        auto end   = std::chrono::steady_clock::now();
-
-        std::chrono::duration<double, std::milli> diff = end - start;
-        std::cout << "Elapsed time = " << std::setw(9) << diff.count() << " ms\n";
-    }
-
+  // Show the help when asked for.
+  if (show_help) {
+    std::cout << cli << "\n";
     return EXIT_SUCCESS;
+  }
+
+  std::cout << "Evaluating the Boys function of order " << order << " on "
+            << npoints << " random points in [" << lower << ", " << upper << "]"
+            << std::endl;
+
+#ifdef _OPENMP
+  std::cout << omp_get_num_procs() << " processors and "
+            << omp_get_max_threads() << " threads available" << std::endl;
+#endif
+
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_real_distribution<double> dist(lower, upper);
+
+  auto v = std::vector<double>(npoints, 0.0);
+  std::generate(v.begin(), v.end(), [&dist, &mt]() { return dist(mt); });
+
+  auto start = std::chrono::steady_clock::now();
+  auto boys = boys_function(order, v);
+  auto end = std::chrono::steady_clock::now();
+
+  std::chrono::duration<double, std::milli> diff = end - start;
+  std::cout << "Elapsed time = " << std::setw(9) << diff.count() << " ms\n ";
+
+  return EXIT_SUCCESS;
 }
