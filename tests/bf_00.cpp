3531f56cd494587c1f3dc844c8cff52d97e8d887
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-04-16 17:20:45 +0200

    Fix tests

--- tests/bf_00.cpp
+++ tests/bf_00.cpp
@@ -11,40 +11,19 @@ TEST_CASE("Boys function of order 0 with small arguments", "[bf_00][small]") {
   auto args = small_args();
   auto res = boys_function(0, args);
 
-  auto bf00ref = std::vector<double>(
-      {1.000000000000000e+00, 8.337643826556670e-01, 7.142807113640670e-01,
-       6.262241865622670e-01, 5.596855024991541e-01, 5.081688518561950e-01,
-       4.673527088040290e-01, 4.343163954040780e-01, 4.070529803295650e-01,
-       3.841594831321780e-01, 3.646377456929750e-01, 3.477649743144210e-01,
-       3.330085519272120e-01, 3.199692619171010e-01, 3.083429405237970e-01});
-
-  REQUIRE_THAT(res, Catch::Approx(bf00ref).margin(1e-13));
+  REQUIRE_THAT(res, Catch::Approx(ref00_small()).margin(1e-13));
 }
 
 TEST_CASE("Boys function of order 0 with medium arguments", "[bf_00][medium]") {
   auto args = medium_args();
   auto res = boys_function(0, args);
 
-  auto bf00ref = std::vector<double>(
-      {2.921751330979280e-01, 2.890511551509060e-01, 2.860251433313420e-01,
-       2.830921042884270e-01, 2.802473905066430e-01, 2.774866706120870e-01,
-       2.748059026617780e-01, 2.722013100791120e-01, 2.696693599402060e-01,
-       2.672067433518620e-01, 2.648103576932870e-01, 2.624772905209140e-01,
-       2.602048049594860e-01, 2.579903264232660e-01, 2.558314305293830e-01});
-
-  REQUIRE_THAT(res, Catch::Approx(bf00ref).margin(1e-13));
+  REQUIRE_THAT(res, Catch::Approx(ref00_medium()).margin(1e-13));
 }
 
 TEST_CASE("Boys function of order 0 with large arguments", "[bf_00][large]") {
   auto args = large_args();
   auto res = boys_function(0, args);
 
-  auto bf00ref = std::vector<double>(
-      {2.530010614452080e-01, 2.018850143830020e-01, 1.729079270949040e-01,
-       1.536450383766990e-01, 1.396542400511970e-01, 1.288997691402010e-01,
-       1.202998345068450e-01, 1.132194394910130e-01, 1.072580727778950e-01,
-       1.021489678366730e-01, 9.770666548820869e-02, 9.379769655720870e-02,
-       9.032322896254140e-02, 8.720830633426380e-02, 8.439490980885930e-02});
-
-  REQUIRE_THAT(res, Catch::Approx(bf00ref).margin(1e-13));
+  REQUIRE_THAT(res, Catch::Approx(ref00_large()).margin(1e-13));
 }
--- tests/bf_01.cpp
+++ tests/bf_01.cpp
@@ -9,51 +9,45 @@
 
 TEST_CASE("Boys function of order 1 with small arguments", "[bf_01][small]") {
   auto args = small_args();
-  auto res = boys_function(1, args);
+  // res contains the values of the Boys function for orders 1 and below
+  // we split it up into by-order arrays
+  const auto res = boys_function(1, args);
 
-  auto bf01ref = std::vector<double>(
-      {3.333333333333330e-01, 2.368110999331860e-01, 1.724584630351420e-01,
-       1.287828242194510e-01, 9.857315239467210e-02, 7.725917727250210e-02,
-       6.191234205110620e-02, 5.063359764909080e-02, 4.217561459545820e-02,
-       3.570786783564010e-02, 3.066934753171250e-02, 2.667537945349410e-02,
-       2.345810584596280e-02, 2.082807327857870e-02, 1.864916958429630e-02});
+  auto res00 = extract<1>(0, res);
 
-  REQUIRE_THAT(res, Catch::Approx(bf01ref).margin(1e-13));
+  REQUIRE_THAT(res00, Catch::Approx(ref00_small()).margin(1e-13));
+
+  auto res01 = extract<1>(1, res);
+
+  REQUIRE_THAT(res01, Catch::Approx(ref01_small()).margin(1e-13));
 }
 
 TEST_CASE("Boys function of order 1 with medium arguments", "[bf_01][medium]") {
   auto args = medium_args();
-  auto npoints = args.size();
-  auto order = 1;
+  // res contains the values of the Boys function for orders 1 and below
+  // we split it up into by-order arrays
+  const auto res = boys_function(1, args);
 
-  auto res = boys_function(order, args);
+  auto res00 = extract<1>(0, res);
 
-  auto bf01ref = std::vector<double>(
-      {1.587359204870060e-02, 1.537066122794410e-02, 1.489361534348180e-02,
-       1.444064554535690e-02, 1.401009952884400e-02, 1.360046570065700e-02,
-       1.321035914321010e-02, 1.283850915421610e-02, 1.248374816837010e-02,
-       1.214500189323060e-02, 1.182128051326880e-02, 1.151167083491540e-02,
-       1.121532926171330e-02, 1.093147550276910e-02, 1.065938692987630e-02});
+  REQUIRE_THAT(res00, Catch::Approx(ref00_medium()).margin(1e-13));
 
-  auto res01 = std::vector<double>(npoints, 0.0);
-  for (auto i = 0; i < npoints; ++i) {
-    for (auto j = 0; j < order+1; ++j) {
-      res01[i] = res[i * (order +1)+ j];
-    }
-  }
-  REQUIRE_THAT(res01, Catch::Approx(bf01ref).margin(1e-13));
+  auto res01 = extract<1>(1, res);
+
+  REQUIRE_THAT(res01, Catch::Approx(ref01_medium()).margin(1e-13));
 }
 
 TEST_CASE("Boys function of order 1 with large arguments", "[bf_01][large]") {
   auto args = large_args();
-  auto res = boys_function(1, args);
+  // res contains the values of the Boys function for orders 1 and below
+  // we split it up into by-order arrays
+  const auto res = boys_function(1, args);
 
-  auto bf01ref = std::vector<double>(
-      {1.030955057373630e-02, 5.238324081627980e-03, 3.290976914560420e-03,
-       2.309062794960850e-03, 1.733973678311360e-03, 1.363441602921520e-03,
-       1.108345628402840e-03, 9.239386281296980e-04, 7.855432311256420e-04,
-       6.785503376954490e-04, 5.938170991139460e-04, 5.253595640036330e-04,
-       4.691141007714830e-04, 4.222344646764010e-04, 3.826739358341310e-04});
+  auto res00 = extract<1>(0, res);
 
-  REQUIRE_THAT(res, Catch::Approx(bf01ref).margin(1e-13));
+  REQUIRE_THAT(res00, Catch::Approx(ref00_large()).margin(1e-13));
+
+  auto res01 = extract<1>(1, res);
+
+  REQUIRE_THAT(res01, Catch::Approx(ref01_large()).margin(1e-13));
 }
