6a40fb25c4a402a9647d9425dc72153ef73b9bcf
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-04-30 12:42:55 +0200

    Generate reference values with SymPy

--- tests/bf.cpp
+++ tests/bf.cpp
@@ -9,7 +9,7 @@ TEMPLATE_TEST_CASE_SIG("Boys function with small arguments", "[bf][small]",
                        ((int32_t order), order), (0), (1), (2), (3), (4), (5),
                        (6), (7), (8), (9), (10), (11), (12), (13), (14), (15),
                        (16), (17), (18), (19), (20), (21), (22), (23), (24),
-                       (25), (26)) {
+                       (25), (26), (27), (28)) {
   auto args = small_args();
   // res contains the values of the Boys function for orders 1 and below
   // we split it up into by-order arrays
@@ -26,7 +26,7 @@ TEMPLATE_TEST_CASE_SIG("Boys function with medium arguments", "[bf][medium]",
                        ((int32_t order), order), (0), (1), (2), (3), (4), (5),
                        (6), (7), (8), (9), (10), (11), (12), (13), (14), (15),
                        (16), (17), (18), (19), (20), (21), (22), (23), (24),
-                       (25), (26)) {
+                       (25), (26), (27), (28)) {
   auto args = medium_args();
   // res contains the values of the Boys function for orders 1 and below
   // we split it up into by-order arrays
@@ -43,7 +43,7 @@ TEMPLATE_TEST_CASE_SIG("Boys function with large arguments", "[bf][large]",
                        ((int32_t order), order), (0), (1), (2), (3), (4), (5),
                        (6), (7), (8), (9), (10), (11), (12), (13), (14), (15),
                        (16), (17), (18), (19), (20), (21), (22), (23), (24),
-                       (25), (26)) {
+                       (25), (26), (27), (28)) {
   auto args = large_args();
   // res contains the values of the Boys function for orders 1 and below
   // we split it up into by-order arrays
